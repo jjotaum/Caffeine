d3305e3c47ad42ea03f0657956a2d6770d52518f
Jota Uribe <jjotaum@gmail.com>
2024-03-12 14:39:26 -0500

    Introduce Parent & Child View Models definitions

--- Sources/Caffeine/Presentation/ViewModel.swift
+++ Sources/Caffeine/Presentation/ViewModel.swift
@@ -20,3 +20,24 @@ public extension ViewModel {
         }
     }
 }
+
+/// A ´ViewModel´ which can be delegated to handle ´ChildViewModel´ actions.
+public protocol ParentViewModel: Caffeine.ViewModel {
+    associatedtype ChildAction
+    func handle(childAction: ChildAction) async
+}
+
+/// A ´ViewModel´ that can delegate its actions to a ´ParentViewModel´.
+public protocol ChildViewModel<Parent>: ViewModel where Parent: ParentViewModel, Parent.ChildAction == Self.Action {
+    associatedtype Parent
+    var parent: Parent { get set }
+}
+
+public extension ParentViewModel {
+    func handle(childAction: ChildAction) {
+        Task {
+            await handle(childAction: childAction)
+        }
+    }
+}
+
