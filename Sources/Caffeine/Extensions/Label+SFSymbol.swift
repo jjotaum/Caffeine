8bd3693338d5c573b39d4d798f628cc5a68e3fba
Jota Uribe <jjotaum@gmail.com>
2024-10-02 14:00:46 -0500

    Add support for custom SFSymbols, Update grids convenience inits to make use of @ViewBuilder (#14)

--- Sources/Caffeine/Extensions/Label+SFSymbol.swift
+++ Sources/Caffeine/Extensions/Label+SFSymbol.swift
@@ -13,14 +13,29 @@ public extension Label where Title == Text, Icon == Image {
     @available(tvOS 16, *)
     @available(watchOS 9, *)
     init(_ localizedString: LocalizedStringResource, symbol: SFSymbol) {
-        self.init(localizedString.key, systemImage: symbol.systemImage)
+        switch symbol.kind {
+        case .system(let systemName):
+            self.init(localizedString.key, systemImage: systemName)
+        case .custom(let name):
+            self.init(localizedString.key, image: name)
+        }
     }
     
     init(_ titleKey: LocalizedStringKey, symbol: SFSymbol) {
-        self.init(titleKey, systemImage: symbol.systemImage)
+        switch symbol.kind {
+        case .system(let systemName):
+            self.init(titleKey, systemImage: systemName)
+        case .custom(let name):
+            self.init(titleKey, image: name)
+        }
     }
     
     init(_ title: String, symbol: SFSymbol) {
-        self.init(title, systemImage: symbol.systemImage)
+        switch symbol.kind {
+        case .system(let systemName):
+            self.init(title, systemImage: systemName)
+        case .custom(let name):
+            self.init(title, image: name)
+        }
     }
 }
--- Sources/Caffeine/Presentation/SFSymbol.swift
+++ Sources/Caffeine/Presentation/SFSymbol.swift
@@ -7,14 +7,27 @@
 
 import Foundation
 
-/// A custom defined to facilitate usage of ´SFSymbols´.
+/// A custom type defined to facilitate usage of ´SFSymbols´.
 /// Please see ´Image+SFSymbol´ & ´Label+SFSymbol´ extensions for reference on how to use it.
 public struct SFSymbol {
-    var systemImage: String
+    var kind: Kind
+}
+
+extension SFSymbol {
+    enum Kind {
+        case system(systemName: String)
+        case custom(imageName: String)
+    }
 }
 
 extension SFSymbol: ExpressibleByStringLiteral {
     public init(stringLiteral value: String) {
-        systemImage = value
+        kind = .system(systemName: value)
+    }
+}
+
+extension SFSymbol {
+    public init(custom imageName: String) {
+        kind = .custom(imageName: imageName)
     }
 }
