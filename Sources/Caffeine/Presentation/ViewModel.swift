37753ef07030c02cd9395ea669c362d51e61bbc1
Jota Uribe <jjotaum@gmail.com>
2026-01-08 17:20:21 -0500

    Introduce Observable ViewModel, previous implementation of ViewModel has been renamed to LegacyViewModel

--- Sources/Caffeine/Presentation/ViewModel.swift
+++ Sources/Caffeine/Presentation/ViewModel.swift
@@ -6,18 +6,21 @@
 //
 
 import Foundation
+import Observation
 
-/// A protocol describing basic structure of viewModels.
-public protocol ViewModel: ObservableObject {
+/// Protocol describing basic structure of ´Observable´ View Models.
+@available(iOS 17.0, macOS 14.0, watchOS 10.0, tvOS 17.0, *)
+public protocol ViewModel: Observable {
     associatedtype Action
     func handle(_ action: Action)
 }
 
-public extension ViewModel {
-    func handle(_ action: Action) async {
-        Task {
-            await handle(action)
-        }
-    }
+@available(iOS, deprecated: 17.0, message: "Use ViewModel on iOS 17+")
+@available(macOS, deprecated: 14.0, message: "Use ViewModel on macOS 14+")
+@available(watchOS, deprecated: 10.0, message: "Use ViewModel on watchOS 10+")
+@available(tvOS, deprecated: 17.0, message: "Use ViewModel on tvOS 17+")
+/// Legacy protocol describing basic structure of ´ObservableObject´ View Models.
+public protocol LegacyViewModel: ObservableObject {
+    associatedtype Action
+    func handle(_ action: Action)
 }
-
